756a1baed5334810a04acca316105bb16e663f91
a-aguadoma <a.aguadomar@gmail.com>
2019-02-19 13:51:05 +0100

    DB Save hecho

--- HealthSport/src/main/java/com/example/AES/Deportista.java
+++ HealthSport/src/main/java/com/example/AES/Deportista.java
@@ -16,6 +16,8 @@ public class Deportista {
 	
 	private String nombre;
 	private String apellido;
+	private String usuario;
+	private String correo;
 	private String pass;
 	
 	@ManyToOne
@@ -40,6 +42,26 @@ public class Deportista {
 	}
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	@Override
+	public String toString() {
+		return "Deportista [nombre=" + nombre + ", apellido=" + apellido + ", pass=" + pass + "]";
 	}
 	
 }
