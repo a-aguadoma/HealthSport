1533158572a6c66bf06ecba9d1f967818e2380b4
a-aguadoma <a.aguadomar@gmail.com>
2019-03-12 14:41:08 +0100

    Seguridad con HTTPS y cambios menores

--- HealthSport/src/main/java/com/example/AES/HealthSportApplication.java
+++ HealthSport/src/main/java/com/example/AES/HealthSportApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,10 +31,18 @@ public class HealthSportApplication {
 	public String login(Model model) {
 		return "login";
 	}
+	@GetMapping("/loginerror")
+	 public String loginerror() {
+	 return "loginerror";
+	 }
 	@RequestMapping("/registroGeneral")
 	public String registroGeneral(Model model) {
 		return "registroGeneral";
 	}
+	@GetMapping("/home")
+	 public String home() {
+	 return "home";
+	 }
 	
 	/*
 	 * @RequestMapping("/calendario") public String calendario(Model model) { return
