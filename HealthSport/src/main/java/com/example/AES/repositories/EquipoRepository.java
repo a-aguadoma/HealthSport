153e082d42d6f42ca00faa3ae2589eb0b163b599
elenagarciamorato <e.garciapi@alumnos.urjc.es>
2019-03-28 12:30:18 +0100

    Cache añadida

--- HealthSport/src/main/java/com/example/AES/repositories/EquipoRepository.java
+++ HealthSport/src/main/java/com/example/AES/repositories/EquipoRepository.java
@@ -1,23 +1,27 @@
 package com.example.AES.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 
 import com.example.AES.models.*;
 
-public interface EquipoRepository extends CrudRepository<Equipo, Long>{
+@CacheConfig(cacheNames="HealthSport")
+public interface EquipoRepository extends JpaRepository<Equipo, Long>{
 	
 	
 	@SuppressWarnings("unchecked")
+	@CacheEvict(allEntries = true)
 	Equipo save(Equipo equipo);
 	
 	@Cacheable
 	Equipo findByNombre(String nombre);
 	
+	@Cacheable
 	Equipo findByClave(String claveEquipo);
 	
 	List<Deportista> findByDeportistas(String nombre);
--- HealthSport/src/main/java/com/example/AES/repositories/EventoRepository.java
+++ HealthSport/src/main/java/com/example/AES/repositories/EventoRepository.java
@@ -2,12 +2,18 @@ package com.example.AES.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 import com.example.AES.models.*;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+
+
+@CacheConfig(cacheNames="HealthSport")
 public interface EventoRepository extends JpaRepository<Evento, Long> {
 	
 	@SuppressWarnings("unchecked")
+	@CacheEvict(allEntries = true)
 	Evento save(Evento evento);
 
 }
